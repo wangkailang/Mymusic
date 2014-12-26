a683d01a1e6873f8b0f91b6111aa73ed1d461649
王开朗 <2418904055@qq.com>
2014-12-26 15:47:38 +0800

    Create list.js

---
+++ js/list.js
@@ -0,0 +1,33 @@
+/*
+ *功能：        播放文件信息
+ *@auther:      wkl
+ *@email        wkl2418904055@163.com
+ *@date         2014-11-17
+ */
+var musicList=[
+		{
+			'name':'化作樱花树',
+			'url':'http://wangkailang.sinaapp.com/music/wkl_song1.mp3',
+			'singer':'Snh48',
+			'singerimg':'./images/snh48.jpg'
+		},
+		{
+			'name':'勇气',
+			'url':'http://wangkailang.sinaapp.com/music/wkl_song2.mp3',
+			'singer':'梁静茹',
+			'singerimg':'./images/ljr.jpg'
+		},
+		{
+			'name':'平凡之路',
+			'url':'http://wangkailang.sinaapp.com/music/wkl_song3.mp3',
+			'singer':'朴树',
+			'singerimg':'./images/pushu1.jpg'
+		},
+		{
+			'name':'且听风吟',
+			'url':'http://wangkailang.sinaapp.com/music/wkl_song4.mp3',
+			'singer':'朴树',
+			'singerimg':'./images/pushu2.jpg'
+		}
+	];
+//var backColor=[];
